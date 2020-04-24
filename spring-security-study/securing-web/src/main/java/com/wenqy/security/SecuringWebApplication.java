abebadaa37dbfa0fcad790fa3ec9a47a6e2d2796
root <wen.qy@qq.com>
2020-04-24 17:06:58 +0800

    spring security web

---
+++ spring-security-study/securing-web/src/main/java/com/wenqy/security/SecuringWebApplication.java
@@ -0,0 +1,12 @@
+package com.wenqy.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuringWebApplication.class, args);
+	}
+}
